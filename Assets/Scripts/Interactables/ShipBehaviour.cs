2e0dfc39d4bcb79db7e0b2356cd2e37124aee634
Lockd <pikagrigoriy@gmail.com>
2022-11-08 23:14:03 +0300

    added tomato seed + small code adjustments

--- Assets/Scripts/Interactables/ShipBehaviour.cs
+++ Assets/Scripts/Interactables/ShipBehaviour.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ShipBehaviour : MonoBehaviour
 {
-    public BackpackItem[] itemsToGiveOnInitialSearch;
     public CharacterBackpack backpack;
-    public BackpackItem[] itemsAfterMeltingIce;
     public HelperTextBehaviour helperTextBehaviour;
+    public BackpackItem[] itemsToGiveOnInitialSearch;
+    public BackpackItem[] itemsAfterMeltingIce;
     BackpackItem ice;
     string[] itemsToRemoveOnMeltingIce = { "Ice" };
 
